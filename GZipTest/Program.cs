7a5204f25f0953240c6add31febe842e3447809c
Daniil Solodyankin <dasolo@rarus.ru>
2019-02-23 15:55:25 +0300

    Создан шаблон класса для потоковой упаковки

--- GZipTest/Program.cs
+++ GZipTest/Program.cs
@@ -7,68 +7,16 @@ namespace GZipTest
 {
     class Program
     {
-        static int BufferSize = 1024 * 1024 * 20; //20mb
-
-        static void Copy(Stream input, Stream output)
-        {
-            var buffer = new byte[BufferSize]; 
-            int bytesRead;
-            input.Seek(0, SeekOrigin.Begin);
-            while ((bytesRead = input.Read(buffer, 0, buffer.Length)) > 0)
-            {
-                output.Write(buffer, 0, bytesRead);
-            }
-        }
-
         static void Main(string[] args)
         {
             var inputFile = @"c:\temp\test.bin";
             var outputFile = @"c:\temp\test.zip";
-            Stopwatch sw = Stopwatch.StartNew();
             var outFile = File.Create(outputFile);
             var inFile = new FileStream(inputFile, FileMode.Open);
-            var innerStream = new MemoryStream();
-            var innerStream2 = new MemoryStream();
-            Console.WriteLine(sw.ElapsedMilliseconds);
-            var zipper = new GZipStream(innerStream, CompressionMode.Compress);
-            var fileZipper = new GZipStream(outFile, CompressionMode.Compress);
-            var fileLength = inFile.Length;
-            Console.WriteLine("длина файла {0}", fileLength);
-            sw.Reset();
-            sw.Start();
-            Copy(inFile, outFile);
-            Console.WriteLine("запись из файла в файл {0}", (double)sw.ElapsedMilliseconds / fileLength);
-            sw.Reset();
-            sw.Start();
-            Copy(inFile, innerStream);
-            Console.WriteLine("запись из файла в поток {0}", (double)sw.ElapsedMilliseconds / fileLength);
-            sw.Reset();
-            sw.Start();
-            Copy(innerStream, innerStream2);
-            Console.WriteLine("запись из потока в поток {0}", (double)sw.ElapsedMilliseconds / fileLength);
-            sw.Reset();
-            sw.Start();
-            Copy(inFile, zipper);
-            Console.WriteLine("упакова из файла в поток {0}", (double)sw.ElapsedMilliseconds / fileLength);
-            sw.Reset();
-            sw.Start();
-            Copy(inFile, fileZipper);
-            Console.WriteLine("упаковка из файла в файл {0}", (double)sw.ElapsedMilliseconds / fileLength);
-            sw.Reset();
-            sw.Start();
-            Copy(innerStream, zipper);
-            Console.WriteLine("упаковка из потока в поток {0}", (double)sw.ElapsedMilliseconds / fileLength);
-            sw.Reset();
-            sw.Start();
-            Copy(innerStream, fileZipper);
-            Console.WriteLine("упаковка из потока в файл {0}", (double)sw.ElapsedMilliseconds / fileLength);
-            sw.Reset();
-            sw.Start();
-            zipper.Close();
+            var Gzip = new ThreadedGZip(inFile, outFile);
+            Gzip.CopyStreams();
             outFile.Close();
             inFile.Close();
-            Console.ReadLine();
-
         }
  
     }
---
+++ GZipTest/ThreadedGZip.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace GZipTest
+{
+    class ThreadedGZip
+    {
+        private Stream Input;
+        private Stream Output;
+        static int BufferSize = 1024 * 1024 * 20;
+        static void Copy(Stream input, Stream output)
+        {
+            var buffer = new byte[BufferSize];
+            int bytesRead;
+            input.Seek(0, SeekOrigin.Begin);
+            while ((bytesRead = input.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                output.Write(buffer, 0, bytesRead);
+            }
+        }
+
+
+        public ThreadedGZip(Stream _input, Stream _output)
+        {
+            this.Input = _input;
+            this.Output = _output;
+        }
+
+        public void CopyStreams()
+        {
+            Copy(Input, Output);
+        } 
+    }
+}
